d8e18af87471b1bd80357489a375297881d5c3c4
Sardor <alijanov.sb@gmail.com>
2022-04-15 09:39:16 +0500

    feat: add todo model

---
+++ backend/models/todoModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const TodoSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Todo", TodoSchema);
